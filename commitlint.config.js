6fa1bb6ad185040503df0ab7965bf5df93c3fe2e
Jeremias Diaz <monitz87@gmail.com>
2022-03-04 19:00:30 -0300

    fix(hashicorp-vault-signing-manager,browser-extension-signing-manager): bump version
    update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-empty': [2, 'never'],
-    'header-max-length': 120,
+    'header-max-length': [2, 'always', 120],
     'scope-enum': [
       2,
       'always',
