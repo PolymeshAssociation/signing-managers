93733bc9ae6ae5ad82a0fe079b56dd55e5d34b41
Mehdi Raash <leaksin@gmail.com>
2023-02-06 11:49:27 -0500

    Update packages/local-signing-manager/src/types/index.ts
    Co-authored-by: polymath-eric <86971143+polymath-eric@users.noreply.github.com>

--- packages/local-signing-manager/src/types/index.ts
+++ packages/local-signing-manager/src/types/index.ts
@@ -2,6 +2,11 @@
  * URI|mnemonic|hex representation of a private key
  */
 export type PrivateKey = ({ uri: string } | { mnemonic: string } | { seed: string }) & {
+  /**
+   * (optional) a specify a derivation path to apply to the secret. This allows for multiple accounts to be created with only one secret
+   *
+   * @note if using this option the account can only be recreated by knowing the path and the secret. Be sure to use a consistent scheme when making the path
+   */
   derivationPath?: string;
 };
 
